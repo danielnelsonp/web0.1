bbd9ed9c3b9737216da8da0ad6eed492f644edd8
daniel <root@DESKTOP-ERA2K45.>
2025-02-16 23:03:53 +0000

    Added supertest as a devDependency

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Jenkins Deployment!');
+app.get("/", (req, res) => {
+  res.send("Hello, Jenkins Deployment!");
 });
 
-app.listen(port, () => {
+// Start the server only if this file is run directly
+if (require.main === module) {
+  app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+  });
+}
 
+// Export app for testing
 module.exports = app;
+
--- server.test.js
+++ server.test.js
@@ -1,10 +1,25 @@
-const request = require('supertest');
-const app = require('./server');
+const request = require("supertest");
+const app = require("./server");
 
-describe('GET /', () => {
-    it('should return Hello, Jenkins Deployment!', async () => {
-        const response = await request(app).get('/');
-        expect(response.text).toBe('Hello, Jenkins Deployment!');
-        expect(response.statusCode).toBe(200);
-    });
+let server;
+let testPort;
+
+beforeAll((done) => {
+  server = app.listen(0, () => {  // Use port 0 to get a random available port
+    testPort = server.address().port;
+    done();
+  });
 });
+
+afterAll((done) => {
+  server.close(done);
+});
+
+describe("GET /", () => {
+  it("should return Hello, Jenkins Deployment!", async () => {
+    const response = await request(`http://localhost:${testPort}`).get("/");
+    expect(response.text).toBe("Hello, Jenkins Deployment!");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
